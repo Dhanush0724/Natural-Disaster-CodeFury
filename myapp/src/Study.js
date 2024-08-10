b6538d13a899800d92cfddc3cf67e6274ed5f21a
Dhanush V <dhanush.v0724@gmail.com>
2024-08-10 11:44:31 +0500

    second commit

---
+++ myapp/src/Study.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Study() {
+  return (
+    <div>
+      <h1>Study Page</h1>
+      <p>Welcome to the Study page.</p>
+    </div>
+  );
+}
+
+export default Study;
